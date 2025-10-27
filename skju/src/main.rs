73ebb98eed2824a198dcb421cee6609a394591e6
Aleksander Plotnikov <aleksander.plotnikov@dataart.com>
2025-10-27 21:43:13 +0200

    Feat: add Sensor and SensorData structures and related functionality; Fix: incorrect clippy configuration, incorrect resolver

--- skju/src/main.rs
+++ skju/src/main.rs
@@ -1,5 +1,3 @@
-use skju_core::get_project_name;
-
 fn main() {
-    println!("Hello, {}!", get_project_name());
+    println!("Hello, SKJU!");
 }
--- skju_core/src/lib.rs
+++ skju_core/src/lib.rs
@@ -1,3 +1,48 @@
-pub fn get_project_name() -> String {
-    String::from("skju")
+use std::collections::VecDeque;
+use std::time::SystemTime;
+
+pub struct Sensor {
+    pub name: String,
+    pub coords: Coord,
+    limit: usize,
+    readings: VecDeque<SensorData>,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Coord {
+    pub x: f32,
+    pub y: f32,
+}
+
+#[derive(Debug)]
+pub struct SensorData {
+    pub value: f64,
+    pub timestamp: SystemTime,
+}
+
+impl Sensor {
+    pub fn new(name: &str, coords: Coord, limit: usize) -> Self {
+        Self {
+            limit,
+            coords,
+            name: name.to_string(),
+            readings: VecDeque::with_capacity(limit),
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.readings.is_empty()
+    }
+
+    pub fn write(&mut self, data: SensorData) {
+        if self.readings.len() == self.limit {
+            self.readings.pop_front();
+        }
+
+        self.readings.push_back(data);
+    }
+
+    pub fn read(&mut self) -> Option<SensorData> {
+        self.readings.pop_front()
+    }
 }
